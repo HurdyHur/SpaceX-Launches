085b449bd943ebeabbbf4880ef821b24069ccf3e
Harry <hurdysteam@gmail.com>
2022-07-10 01:58:07 +0100

    neaten MainActivity

--- app/src/main/java/com/harry/spacexlaunches/MainActivity.kt
+++ app/src/main/java/com/harry/spacexlaunches/MainActivity.kt
@@ -3,12 +3,9 @@ package com.harry.spacexlaunches
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.harry.spacexlaunches.launches.LaunchesViewModel
 import com.harry.spacexlaunches.launches.ui.LaunchesScreen
 import com.harry.spacexlaunches.ui.theme.SpaceXLaunchesTheme
@@ -32,13 +29,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SpaceXLaunchesTheme {
-
     }
 }
